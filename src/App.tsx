f1bb9320839ccc4b3a531bba6966f8ce9052ef6c
Changes729 <changes0729@163.com>
2023-04-29 17:51:11 +0800

    [BUG #1] 使用 XMLHttpRequest 解决POST跳转问题

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    const url = "http://localhost:8080/upload/"
+    console.log("submit")
+    event.preventDefault()
+
+    // collect files
+    let files = (document.querySelector('[name=file]') as HTMLInputElement).files
+
+    if (files && files.length > 0) {
+      const formData = new FormData()
+      formData.append('File', files[0])
+
+      // post form data
+      const xhr = new XMLHttpRequest()
+
+      // log response
+      xhr.onload = () => {
+        console.log(xhr.responseText)
+      }
+
+      // create and send the reqeust
+      xhr.open('POST', url)
+      xhr.send(formData)
+    }
+    else {
+      console.log("file is empty")
+    }
+  }
+
   return (
     <div className="App">
-      <form action="http://localhost:8080/upload" method="post" encType="multipart/form-data">
-        <p />File Name: <input type="text" name="FileName" placeholder="file name" />
-        <p /><input type="file" name="FileName" />
-        <p /><button type="submit">Submit</button>
+      <form method="POST" encType="multipart/form-data" onSubmit={submit}>
+        <input type="file" name="file" />
+        <button type="submit" role="button">Upload File</button>
       </form>
     </div>
   );
