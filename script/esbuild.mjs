60d540cde2bb05c1318f7b9b5c8b2276c0386191
Changes729 <changes729@163.com>
2024-03-01 16:49:27 +0800

    [INTERNAL] update project to esbuild version

---
+++ script/esbuild.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from "esbuild";
+import fs from "node:fs";
+
+const HTML_DIR = "public/html/";
+const OUT_DIR = "dist/";
+
+fs.rmSync(OUT_DIR, { recursive: true, force: true });
+fs.cpSync(HTML_DIR, OUT_DIR, {recursive: true});
+
+let ctx = await esbuild.context({
+  entryPoints: ["src/index.tsx"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  loader: { ".htm": "file" },
+  outdir: `${OUT_DIR}/js`,
+});
+
+let { host, port } = await ctx.serve({
+  servedir: OUT_DIR,
+});
+await ctx.watch();
+
+console.log(`[serve] listening at http://${host}:${port}`);
