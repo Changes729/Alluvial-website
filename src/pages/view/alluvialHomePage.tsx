4dd26ae68a0c8139f0c2edbdf1361392569b0cb1
asuki <changes729@163.com>
2025-12-13 16:20:14 +0800

    move hash to utils

--- src/pages/view/alluvialHomePage.tsx
+++ src/pages/view/alluvialHomePage.tsx
@@ -16,6 +16,7 @@ import {
 } from "react-complex-tree";
 import { useLocation, useNavigate, useParams } from "react-router";
 import { Menu } from "../widget/AlluvialContent/menu";
+import { hash } from "../../utils/hash";
 
 interface AlluvialContent extends MilkdownContent {
   url: string;
@@ -49,18 +50,6 @@ export const AlluvialHomepage: React.FC<{}> = ({}) => {
 
   function _location(): string {
     return "/" + params["*"];
-  }
-
-  function hash(str: string) {
-    let hash = 2166136261; // FNV-1a 算法的初始值
-    for (let i = 0; i < str.length; i++) {
-      // 逐个字符处理，并将结果限制在 32 位带符号整数
-      hash ^= str.charCodeAt(i);
-      hash *= 16777619;
-      hash &= 0xffffffff; // 确保结果为 32 位整数
-    }
-    // 转换为无符号整数
-    return hash >>> 0;
   }
 
   /** componentDidMount */
